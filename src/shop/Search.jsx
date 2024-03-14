e6124617e674096fa0c10ce81bf98d8f94b24a1a
engProgrammer2001 <askushwaha613@gmail.com>
2024-03-14 09:28:40 +0530

    search functionality

--- src/shop/Search.jsx
+++ src/shop/Search.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Search = (products, GridList) => {
   const [searchTerm, setSearchTerm] = useState("");
-  const filterProducts = products.filter((product) => product.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
-    <div className='widget widget-search'>
-      <form className='search-wrapper mb-3'>
-        <input type='text' name='search' id='search' placeholder='Search.....' defaultValue={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
-        <button type='submit'>
+    <div className="widget widget-search">
+      <form className="search-wrapper mb-3">
+        <input type="text" name="search" id="search" placeholder="searech...." defaultValue={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+        <button type="submit">
           <i className="icofont-search"></i>
         </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
